ae6a3d72c2546be584ee793b1d69a45479f9f4b2
William Whitaker <william@williamwhitaker.dev>
2023-04-26 16:29:34 -0400

    sets options for grid item padding

--- mote-app/src/components/m-grid-item.js
+++ mote-app/src/components/m-grid-item.js
@@ -32,7 +32,30 @@ export class MGridItem extends LitElement {
 
   connectedCallback() {
     super.connectedCallback();
-    this.style.setProperty('--padding', this.padding);
+    let option;
+    switch (this.padding) {
+      case 'none':
+        option = 0;
+        break;
+      case 'scant':
+        option = 'var(--space-3xs)';
+        break;
+      case 'narrow':
+        option = 'var(--space-s)';
+        break;
+      case 'standard':
+        option = 'var(--space-m)';
+        break;
+      case 'wide':
+        option = 'var(--space-l)';
+        break;
+      case 'jumbo':
+        option = 'var(--space-2xl)';
+        break;
+      default:
+        option = '1rem';
+    }
+    this.style.setProperty('--padding', option);
   }
 
   render() {
