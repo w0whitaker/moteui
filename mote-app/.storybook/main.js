4cdca81510a8bd8c99f0a3b3d8ad4fbe6e92d45e
William Whitaker <william@williamwhitaker.dev>
2023-06-12 13:47:27 -0400

    removes addon-mdx-gfm

--- mote-app/.storybook/main.js
+++ mote-app/.storybook/main.js
@@ -1,15 +1,14 @@
 /** @type { import('@storybook/web-components-vite').StorybookConfig } */
 
 const config = {
-  stories: ['../docs/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: ["../docs/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-mdx-gfm',
-    '@storybook/addon-docs',
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-docs",
   ],
   framework: {
-    name: '@storybook/web-components-vite',
+    name: "@storybook/web-components-vite",
     options: {},
   },
   docs: {
