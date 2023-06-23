4d1b8ba1df4c7bfb78adeade65829b3f04786c4a
William Whitaker <william@williamwhitaker.dev>
2023-06-23 13:59:30 -0400

    moves css to module

--- src/components/Button/m-button.ts
+++ src/components/Button/m-button.ts
@@ -1,6 +1,9 @@
-import { LitElement, html, css } from 'lit';
+/** @format */
+
+import { LitElement, html } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
 import { classMap } from 'lit/directives/class-map.js';
+import { buttonStyles } from './button-styles';
 
 export const Sizes = {
   Small: 'sm',
@@ -27,71 +30,7 @@ export class MButton extends LitElement {
     return;
   };
 
-  static styles = css`
-    /* https://spin.atomicobject.com/2015/07/14/css-responsive-square/ */
-    :host {
-      display: block;
-    }
-
-    .m-button {
-      border-style: solid;
-      border-width: 0.375em;
-      border-color: #e6ebff;
-      border-radius: 0.25em;
-      padding: 0;
-      background-color: #00171f;
-      position: relative;
-      font-family: 'Fredoka', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-        Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue',
-        sans-serif;
-      color: #e6ebff;
-    }
-
-    .m-button:after {
-      content: '';
-      display: block;
-      padding-bottom: 100%;
-    }
-
-    .m-button--sm {
-      width: 50%;
-      font-size: 1em;
-    }
-
-    .m-button--md {
-      width: 75%;
-      font-size: 1.5em;
-    }
-
-    .m-button--lg {
-      width: 100%;
-      font-size: 1.75em;
-    }
-
-    .m-button--content {
-      position: absolute;
-      width: 100%;
-      height: 100%;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      top: 0;
-      left: 0;
-      color: #e6ebff;
-    }
-
-    .m-button[disabled] {
-      /* filter: brightness(0.5); */
-      filter: invert(25%);
-    }
-
-    .m-button[disabled] > * {
-      text-decoration: underline;
-      text-underline-offset: -0.3em;
-      text-decoration-skip-ink: none;
-      /* text-decoration: line-through; */
-    }
-  `;
+  static styles = [buttonStyles];
 
   render() {
     const classes = {
