a65a2b1a647b52a969eb3d7e84d57974b1eec08b
William Whitaker <william@williamwhitaker.dev>
2023-07-09 05:41:59 -0400

    Update m-accordion.ts

--- src/components/Accordion/m-accordion.ts
+++ src/components/Accordion/m-accordion.ts
@@ -10,6 +10,7 @@ import { accordionStyles } from './accordion-styles';
  * The Accordion element.
  *
  * @slot - This element has a slot
+ * just a dummy change
  */
 
 @customElement('m-accordion')
