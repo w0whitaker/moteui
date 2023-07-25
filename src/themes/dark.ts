a2d02e7d53600bb86619be38c18e87f996a20bb6
William Whitaker <william@williamwhitaker.dev>
2023-07-24 21:52:48 -0400

    Update dark.ts

--- src/themes/dark.ts
+++ src/themes/dark.ts
@@ -6,12 +6,8 @@ export const Dark = css`
   .dark-theme {
     background-color: var(--dark, #00171f);
     border-color: var(--dark, #00171f);
-    outline-color: var(--light, #e6ebff);
     color: var(--light, #e6ebff);
-    filter: drop-shadow(0em 0.125em 0 var(--dark, #00171f)) saturate(1);
-  }
-
-  .dark-theme:hover {
-    filter: drop-shadow(-0.125em 0 0 var(--dark, #00171f));
+    box-shadow: -0.125em 0.125em var(--light, #e6ebff),
+      -0.375em 0.25em var(--dark, #00171f);
   }
 `;
