7d4262997c06560b5fab3ea3a9b2dca5c090a852
William Whitaker <william@williamwhitaker.dev>
2023-07-24 21:52:43 -0400

    Update light.ts

--- src/themes/light.ts
+++ src/themes/light.ts
@@ -6,7 +6,6 @@ export const Light = css`
   .light-theme {
     background-color: var(--light, #e6ebff);
     border-color: var(--dark, #00171f);
-    /* outline-color: var(--light, #e6ebff); */
     color: var(--dark, #00171f);
     box-shadow: -0.125em 0.125em var(--light, #e6ebff),
       -0.375em 0.25em var(--dark, #00171f);
