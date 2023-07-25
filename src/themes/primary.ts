8c0cf34fdcd30fe630b6dc042e1f36e583cfad40
William Whitaker <william@williamwhitaker.dev>
2023-07-24 21:54:23 -0400

    Update primary.ts

--- src/themes/primary.ts
+++ src/themes/primary.ts
@@ -6,7 +6,8 @@ export const Primary = css`
   .primary-theme {
     background-color: var(--primary, #1e88e5);
     border-color: var(--primary, #1e88e5);
-    outline-color: var(--light, #e6ebff);
     color: var(--light, #e6ebff);
+    box-shadow: -0.125em 0.125em var(--light, #e6ebff),
+      -0.375em 0.25em var(--primary, #1e88e5);
   }
 `;
