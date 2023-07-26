dadd7202c49a615f911f9daaac1f2c0531586228
William Whitaker <william@williamwhitaker.dev>
2023-07-26 09:07:49 -0400

    Update primary.ts

--- src/themes/primary.ts
+++ src/themes/primary.ts
@@ -7,11 +7,10 @@ export const Primary = css`
     background-color: var(--primary, #1e88e5);
     border-color: var(--primary, #1e88e5);
     color: var(--light, #e6ebff);
-    box-shadow: -0.375em 0.25em var(--dark, #00171f);
+    box-shadow: -0.375em 0.25em var(--primary, #1e88e5);
   }
 
-  .primary-theme:hover::before,
-  .primary-theme:hover * {
+  .primary-theme:hover {
     filter: brightness(1.25);
     transition: filter 0.125s ease-in-out;
   }
