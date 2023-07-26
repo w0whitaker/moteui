cad374dae3e8dfb1484cd85b7814ad00afed5f1b
William Whitaker <william@williamwhitaker.dev>
2023-07-26 14:31:28 -0400

    Update secondary.ts

--- src/themes/secondary.ts
+++ src/themes/secondary.ts
@@ -7,7 +7,7 @@ export const Secondary = css`
     background-color: var(--secondary, #ff4081);
     border-color: var(--secondary, #ff4081);
     color: var(--light, #e6ebff);
-    box-shadow: -0.375em 0.25em var(--secondary, #ff4081);
+    box-shadow: -6px 4px var(--secondary, #ff4081);
   }
 
   .secondary-theme:hover {
