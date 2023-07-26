9a68beb376db646cf4c5e3f94dea140c2dfd1cad
William Whitaker <william@williamwhitaker.dev>
2023-07-26 09:04:41 -0400

    Update primary.ts

--- src/themes/primary.ts
+++ src/themes/primary.ts
@@ -7,6 +7,12 @@ export const Primary = css`
     background-color: var(--primary, #1e88e5);
     border-color: var(--primary, #1e88e5);
     color: var(--light, #e6ebff);
-    box-shadow: -0.375em 0.25em var(--primary, #1e88e5);
+    box-shadow: -0.375em 0.25em var(--dark, #00171f);
+  }
+
+  .primary-theme:hover::before,
+  .primary-theme:hover * {
+    filter: brightness(1.25);
+    transition: filter 0.125s ease-in-out;
   }
 `;
