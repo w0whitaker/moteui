4ce3a1ba32fd1e463876a354f8e7d71e4638699f
William Whitaker <william@williamwhitaker.dev>
2023-07-26 14:29:22 -0400

    Update primary.ts

--- src/themes/primary.ts
+++ src/themes/primary.ts
@@ -7,7 +7,7 @@ export const Primary = css`
     background-color: var(--primary, #1e88e5);
     border-color: var(--primary, #1e88e5);
     color: var(--light, #e6ebff);
-    box-shadow: -0.375em 0.25em var(--primary, #1e88e5);
+    box-shadow: -6px 4px var(--primary, #1e88e5);
   }
 
   .primary-theme:hover {
