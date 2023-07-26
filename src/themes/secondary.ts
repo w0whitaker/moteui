aa571cda3c5e730851a7cae6191f1ceb73131112
William Whitaker <william@williamwhitaker.dev>
2023-07-26 09:08:46 -0400

    Update secondary.ts

--- src/themes/secondary.ts
+++ src/themes/secondary.ts
@@ -8,4 +8,10 @@ export const Secondary = css`
     border-color: var(--secondary, #ff4081);
     color: var(--light, #e6ebff);
     box-shadow: -0.375em 0.25em var(--secondary, #ff4081);
+  }
+
+  .secondary-theme:hover {
+    filter: brightness(1.125);
+    transition: filter 0.125s ease-in-out;
+  }
 `;
