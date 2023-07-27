ead64a44feaefc124ef134308b098abd02e1d020
William Whitaker <william@williamwhitaker.dev>
2023-07-27 14:36:19 -0400

    Update accordion-item-styles.ts

--- src/components/Accordion/accordion-item-styles.ts
+++ src/components/Accordion/accordion-item-styles.ts
@@ -6,8 +6,6 @@ export const accordionItemStyles = css`
   :host {
     display: flex;
     justify-content: space-between;
-    padding-inline: var(--space-s, 1rem);
-    padding-block: var(--space-m, 1.5rem);
     border-style: solid;
     border-width: 2px;
     border-radius: 0.25em;
@@ -16,19 +14,36 @@ export const accordionItemStyles = css`
     box-shadow: -1px 1px var(--light, #e6ebff), -6px 4px var(--dark, #00171f);
   }
 
+  h3,
+  p {
+    padding: 0;
+    margin: 0;
+  }
+
+  .top {
+    padding-block-end: 1.25rem;
+    background-color: var(--dark, #00171f);
+    color: var(--light, #e6ebff);
+  }
+
   .m-accordion-item--content {
-    flex-basis: auto;
+    display: flex;
+    flex-direction: column;
+    justify-content: stretch;
+    flex-basis: 80%;
   }
 
   .m-accordion-item--button {
+    display: flex;
+    flex-direction: column;
+    justify-content: stretch;
+    align-items: center;
     /* this padding-block-end squeezes the button's box-shadow into this container */
     padding-block-end: 4px;
     /* ditto for this padding-inline-start */
     padding-inline-start: 6px;
-    display: flex;
-    justify-content: center;
-    align-items: flex-start;
     flex-basis: auto;
+    flex-grow: 1;
   }
 
   .m-accordion-item--border {
