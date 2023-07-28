bf9a382cc303ee4d536ba6b7c934c1aae318cbc5
William Whitaker <william@williamwhitaker.dev>
2023-07-27 20:49:31 -0400

    Update accordion-item-styles.ts

--- src/components/Accordion/accordion-item-styles.ts
+++ src/components/Accordion/accordion-item-styles.ts
@@ -4,11 +4,10 @@ import { css } from 'lit';
 
 export const accordionItemStyles = css`
   :host {
-    display: grid;
+    display: block;
     border-style: solid;
     border-width: 2px;
     border-radius: 0.25em;
-    box-shadow: -1px 1px var(--light, #e6ebff), -6px 4px var(--dark, #00171f);
   }
 
   h3,
@@ -24,6 +23,28 @@ export const accordionItemStyles = css`
     padding-inline-start: 6px;
   }
 
+  .m-accordion-item {
+    position: relative;
+    border-color: inherit;
+  }
+
+  .m-accordion-item::before {
+    content: '';
+    position: absolute;
+    top: -2px;
+    left: -2px;
+    display: block;
+    width: 100%;
+    height: 100%;
+    border-style: solid;
+    border-width: 2px;
+    border-radius: 0.25em;
+    border-color: inherit;
+    background-color: transparent;
+    box-shadow: -1px 1px var(--light, #e6ebff);
+    z-index: 1;
+  }
+
   .top {
     display: flex;
     flex-direction: row;
@@ -35,5 +56,10 @@ export const accordionItemStyles = css`
   .bottom {
     padding-block: var(--space-xs, 0.75rem);
     padding-inline: var(--space-m, 1.5rem);
+  }
+
+  .bottom {
+    background-color: var(--light, #e6ebff);
+    color: var(--dark, #00171f);
   }
 `;
